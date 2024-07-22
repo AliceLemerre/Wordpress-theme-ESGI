3c1a410765edb988411716a30a3e54dbc7db76a1
Alice Lemerre <al.lemerre@laposte.net>
2024-07-22 09:33:29 +0200

    fix search

--- ESGI-theme/search.php
+++ ESGI-theme/search.php
@@ -4,7 +4,7 @@ get_header();
 $search_query = get_search_query();
 
 if (!isset($paged)) {
-    $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
+    $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1; 
 }
 
 $args = [
@@ -24,7 +24,7 @@ $the_query = new WP_Query($args);
         <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
             <div class="post-item">
                 <a href="<?= esc_url(get_permalink()) ?>">
-                    <h2><?= esc_html(get_the_title()) ?></h2>
+                    <h3><?= esc_html(get_the_title()) ?></h3>
                     <time><?= esc_html(get_the_date('j F Y')) ?></time>
                 </a>
                 <p><?= esc_html(get_the_excerpt()) ?></p>
@@ -32,7 +32,7 @@ $the_query = new WP_Query($args);
         <?php endwhile; ?>
     </div>
     <?php
-    $big = 999999999; // need an unlikely integer
+    $big = 999999999; 
 
     echo paginate_links([
         'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
